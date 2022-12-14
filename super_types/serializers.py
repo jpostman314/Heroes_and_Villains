270b70ad96f177c99fbbf0ef00ffbaf3c8ab5927
Josh <jpostman314@yahoo.com>
2022-12-14 11:34:44 -0500

    Updated Project URLs + Created First Views Function + Created Supers App URL File

---
+++ super_types/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import SuperTypes
+
+
+class SuperTypesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SuperTypes
+        fields = ['id', 'type']
