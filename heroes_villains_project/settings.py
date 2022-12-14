8e4a9074655153a55e83722b867bd2de46e3ff73
Josh <jpostman314@yahoo.com>
2022-12-14 10:47:31 -0500

    Created Supers Model for supers App + Created SuperTypes Model for super_types App + Registered Supers, SuperTypes and Rest Framework in Project Settings Installed Apps Section

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4ll5#ya7ee%*a2wm4zhz^)vo04c)99e)a00^b+t3d(^97uq%=%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'super_types',
+    'supers'
 ]
 
 MIDDLEWARE = [
