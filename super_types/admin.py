5d1794f48921c7a77f6038438ece52151e2e8154
Josh <jpostman314@yahoo.com>
2023-01-06 20:59:44 -0500

    Finished serializers and created query params

--- super_types/admin.py
+++ super_types/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import SuperTypes
 # Register your models here.
+
+
+admin.site.register(SuperTypes)
--- supers/views.py
+++ supers/views.py
@@ -1,9 +1,36 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SupersSerializer
+from .models import Supers
+from rest_framework import status
 
-
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
+    if request.method == 'GET':
+        super_type_param = request.query_params.get("super_type")
+        supers = Supers.objects.all()
+        if super_type_param:
+            supers = supers.filter(super_type__type=super_type_param)
+        serializer = SupersSerializer(supers, many=True)
+        return Response(serializer.data) 
+    elif request.method == 'POST':
+        serializer = SupersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-    return Response('ok')
+@api_view(['GET', 'PUT', 'DELETE'])
+def supers_detail(request, pk):
+    super = get_object_or_404(Supers, pk=pk)
+    if request.method == 'GET':
+        serializer = SupersSerializer(super);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SupersSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
